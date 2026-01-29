124f9732adce87c49535652a28c6f18a0dbdd5d8
linhphung211 <lphungtuan2004@gmail.com>
2026-01-29 17:07:09 +0700

    All done

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,32 @@ import tasksRouter from './routes/tasksRouters.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const _dirname = path.resolve();
+
 const app = express();
 
 
 // middleware
 app.use(express.json());
-app.use(cors({ origin: 'http://127.0.0.1:5173' }));
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: 'http://127.0.0.1:5173' }));
+}
 
 app.use("/api/tasks", tasksRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, '../frontend/dist/index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
